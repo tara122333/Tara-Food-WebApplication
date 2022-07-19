3b031886730fa06a36c1d0041debd43a20e1f640
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-07-20 00:14:51 +0530

    add food api validation

--- Server/API/food/index.js
+++ Server/API/food/index.js
@@ -7,6 +7,10 @@ import { FoodModel } from "../../database/allModels";
 
 const Router = express.Router();
 
+// import data validation
+
+import { ValidateRestaurantId , Validatecategory} from "../../validation/food";
+
 
 /*
 route      ==> /r
@@ -18,6 +22,7 @@ Access     ==> public
 
 Router.get("/r/:_id", async(req,res)=>{
     try {
+        await ValidateRestaurantId(req.params);
         const {_id} = req.params;
         const foods = await FoodModel.find({restaurant : _id});
         if(!foods) return res.status(404).json({message : "foos Not found"});
@@ -38,6 +43,7 @@ Access     ==> public
 
 Router.get("/r/:category", async(req,res)=>{
     try {
+        await Validatecategory(req.params);
         const {category} = req.params;
         const foods = await FoodModel.find({category : {$regex : category , $options : "i"}});
         if(!foods) return res.status(404).json({message : "foos Not found"});
---
+++ Server/validation/food.js
@@ -0,0 +1,14 @@
+import Joi from 'joi';
+
+export const ValidateRestaurantId = (resId) =>{
+    const Schema = Joi.object({
+        _id:Joi.string().required(),
+    });
+    return Schema.validateAsync(resId);
+} 
+export const Validatecategory = (category) =>{
+    const Schema = Joi.object({
+        _id:Joi.string().required(),
+    });
+    return Schema.validateAsync(category);
+} 
