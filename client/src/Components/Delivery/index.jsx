256114068502e2e6145455418445bd2d282ea45a
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-07-28 22:35:25 +0530

    Restaurant Section dynamically Done

--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -1,9 +1,141 @@
-import React from "react";
+import React, { useState } from "react";
 import DeliveryCarousel from "./DeliveryCarousel";
 import DeliveryBrands from "./Brands";
 import RestaurantCard from "../restaurantCard";
 
 const Delivery = () =>{
+
+    const [restaurantList, setRestaurantList] = useState([
+        {
+            isPro : true,
+            isOff : 30,
+            durationOfdelivery : 40,
+            images : [
+                "https://b.zmtcdn.com/data/pictures/0/60340/e4432457273c33bcd1f287e6ec473749.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+                "https://b.zmtcdn.com/data/pictures/chains/0/60340/773037a54535261f43738546f29aedf1.jpg?fit=around|300:273&crop=300:273;*,*"
+            ],
+            name : "Auntie Fung's",
+            restaurantReviewValue : 4.1,
+            cuisine : ["Asian", "Thai", "Chinese", "Vietnamese"],
+            averageCost : 150
+        },
+        {
+            isPro : true,
+            isOff : 30,
+            durationOfdelivery : 40,
+            images : [
+                "https://b.zmtcdn.com/data/pictures/chains/8/51418/4f4440d6f4e39151f92a850c27ac13f7.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+                "https://b.zmtcdn.com/data/pictures/chains/8/51418/6d4d04396c5d7f59c760fceeaed5693f.jpg?fit=around|300:273&crop=300:273;*,*"
+            ],
+            name : "Pizza Hut",
+            restaurantReviewValue : 4.1,
+            cuisine : ["Pizza", "Fast Food", "Desserts"],
+            averageCost : 150
+        },
+        {
+            isPro : true,
+            isOff : 30,
+            durationOfdelivery : 40,
+            images : [
+                "https://b.zmtcdn.com/data/pictures/chains/5/61555/572999873b161d836dce1cb64827ef6f_featured_v2.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+                "https://b.zmtcdn.com/data/pictures/chains/5/61555/298cd1d512282f56a9aef6aa4601ad75.jpg?fit=around|300:273&crop=300:273;*,*"
+            ],
+            name : "Burger King",
+            restaurantReviewValue : 4.1,
+            cuisine : ["Burger", "Fast Food", "Beverages"],
+            averageCost : 150
+        },
+        {
+            isPro : true,
+            isOff : 30,
+            durationOfdelivery : 40,
+            images : [
+                "https://b.zmtcdn.com/data/pictures/0/60340/e4432457273c33bcd1f287e6ec473749.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+                "https://b.zmtcdn.com/data/pictures/chains/0/60340/773037a54535261f43738546f29aedf1.jpg?fit=around|300:273&crop=300:273;*,*"
+            ],
+            name : "Auntie Fung's",
+            restaurantReviewValue : 4.1,
+            cuisine : ["Asian", "Thai", "Chinese", "Vietnamese"],
+            averageCost : 150
+        },
+        {
+            isPro : true,
+            isOff : 30,
+            durationOfdelivery : 40,
+            images : [
+                "https://b.zmtcdn.com/data/pictures/chains/8/51418/4f4440d6f4e39151f92a850c27ac13f7.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+                "https://b.zmtcdn.com/data/pictures/chains/8/51418/6d4d04396c5d7f59c760fceeaed5693f.jpg?fit=around|300:273&crop=300:273;*,*"
+            ],
+            name : "Pizza Hut",
+            restaurantReviewValue : 4.1,
+            cuisine : ["Pizza", "Fast Food", "Desserts"],
+            averageCost : 150
+        },
+        {
+            isPro : true,
+            isOff : 30,
+            durationOfdelivery : 40,
+            images : [
+                "https://b.zmtcdn.com/data/pictures/chains/5/61555/572999873b161d836dce1cb64827ef6f_featured_v2.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+                "https://b.zmtcdn.com/data/pictures/chains/5/61555/298cd1d512282f56a9aef6aa4601ad75.jpg?fit=around|300:273&crop=300:273;*,*"
+            ],
+            name : "Burger King",
+            restaurantReviewValue : 4.1,
+            cuisine : ["Burger", "Fast Food", "Beverages"],
+            averageCost : 150
+        },
+        {
+            isPro : true,
+            isOff : 30,
+            durationOfdelivery : 40,
+            images : [
+                "https://b.zmtcdn.com/data/pictures/0/60340/e4432457273c33bcd1f287e6ec473749.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+                "https://b.zmtcdn.com/data/pictures/chains/0/60340/773037a54535261f43738546f29aedf1.jpg?fit=around|300:273&crop=300:273;*,*"
+            ],
+            name : "Auntie Fung's",
+            restaurantReviewValue : 4.1,
+            cuisine : ["Asian", "Thai", "Chinese", "Vietnamese"],
+            averageCost : 150
+        },
+        {
+            isPro : true,
+            isOff : 30,
+            durationOfdelivery : 40,
+            images : [
+                "https://b.zmtcdn.com/data/pictures/chains/8/51418/4f4440d6f4e39151f92a850c27ac13f7.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+                "https://b.zmtcdn.com/data/pictures/chains/8/51418/6d4d04396c5d7f59c760fceeaed5693f.jpg?fit=around|300:273&crop=300:273;*,*"
+            ],
+            name : "Pizza Hut",
+            restaurantReviewValue : 4.1,
+            cuisine : ["Pizza", "Fast Food", "Desserts"],
+            averageCost : 150
+        },
+        {
+            isPro : true,
+            isOff : 30,
+            durationOfdelivery : 40,
+            images : [
+                "https://b.zmtcdn.com/data/pictures/chains/5/61555/572999873b161d836dce1cb64827ef6f_featured_v2.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+                "https://b.zmtcdn.com/data/pictures/chains/5/61555/298cd1d512282f56a9aef6aa4601ad75.jpg?fit=around|300:273&crop=300:273;*,*"
+            ],
+            name : "Burger King",
+            restaurantReviewValue : 4.1,
+            cuisine : ["Burger", "Fast Food", "Beverages"],
+            averageCost : 150
+        },
+        {
+            isPro : true,
+            isOff : 30,
+            durationOfdelivery : 40,
+            images : [],
+            name : "",
+            restaurantReviewValue : 4.1,
+            cuisine : [],
+            averageCost : 150
+        },
+    ]);
+
+
     return(
         <>
             <div className="bg-gray-100 py-3 lg:pb-10">
@@ -15,12 +147,12 @@ const Delivery = () =>{
             <div className="lg:container lg:mx-auto lg:w-full lg:px-24">
                 <h1 className="text-xl px-3 mt-4 mb-2 md:mt-8 md:text-3xl md:font-semibold">Best Food in Bengaluru</h1>
                 <div className="flex flex-wrap ">
-                    <RestaurantCard />
-                    <RestaurantCard />
-                    <RestaurantCard />
-                    <RestaurantCard />
-                    <RestaurantCard />
-                    <RestaurantCard />
+                    
+                    {
+                        restaurantList.map((items)=>(
+                            <RestaurantCard {...items} key={items._id}/>
+                        ))
+                    }
                 </div>
             </div>
         </>
--- client/src/Components/restaurantCard.jsx
+++ client/src/Components/restaurantCard.jsx
@@ -1,30 +1,40 @@
 import React from "react";
 import {FiStar} from 'react-icons/fi';
-const RestaurantCard = () =>{
+const RestaurantCard = (props) =>{
     return(
         <>
             <div className="bg-white hover:shadow-lg p-4 mb-4 w-full rounded-2xl transition duration-700 ease-in-out md:w-1/2 lg:w-1/3">
                 <div className="w-full h-56 lg:h-64 relative">
-                    <div className="flex justify-between items-end absolute w-full bottom-0">
+                    <div className="flex justify-between items-end absolute w-full bottom-3">
                         <div className="flex flex-col gap-2 items-star">
-                            <span className="bg-red-500 text-white px-2 py-1 rounded text-sm">Pro Extra 20% OFF</span>
-                            <span className="bg-blue-600 text-white px-2 py-1 rounded text-sm">30% OFF</span>
+                            {
+                                props.isPro && (
+                                    <span className="bg-red-500 text-white px-2 py-1 rounded text-sm">Pro Extra 20% OFF</span>
+                                )
+                            }
+                            {
+                                props.isOff && (
+                                    <span className="bg-blue-600 text-white px-2 py-1 rounded text-sm">{`₹${props.isOff}% OFF`}</span>
+                                )
+                            }
+                            
+                            
                         </div>
-                        <span className="bg-white bg-opacity-75 p-1 rounded	mr-3">43 min</span>
+                        <span className="bg-white bg-opacity-75 p-1 rounded	mr-3">{props.durationOfdelivery} min</span>
                     </div>
-                    <img src="https://b.zmtcdn.com/data/pictures/chains/7/58607/4307315f24c7474ba57b31f38b4abfaa_o2_featured_v2.jpg?output-format=webp" alt="restaurant" className="w-full h-full rounded-lg lg:rounded-xl"/>
+                    <img src={props.images.length && props.images[0]} alt="restaurant" className="w-full h-full rounded-lg lg:rounded-xl"/>
                 </div>
 
                 <div className="flex flex-col gap-2 my-2">
                     <div className="flex justify-between items-center">
-                        <h4 className="text-sm font-medium">Auntie Fung's</h4>
+                        <h4 className="text-sm font-medium">{props.name}</h4>
                         <span className="flex gap-2 justify-center items-center bg-green-500 rounded-sm lg:rounded-md px-2 text-white font-bold">
-                            4.1 <FiStar />
+                        {props.restaurantReviewValue} <FiStar />
                         </span>
                     </div>
                     <div className="flex justify-between items-center text-gray-500">
-                        <h3>Asian, Thai</h3>
-                        <span>₹150 for One</span>
+                        <p>{props.cuisine.join(", ")}</p>
+                        <p>₹ {props.averageCost} for one</p>
                     </div>
                 </div>
             </div>
