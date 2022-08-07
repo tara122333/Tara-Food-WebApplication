3f602dc3844a033a5b7802f6db69584c85eb00a6
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-08-07 22:33:16 +0530

    add aside MenuListContainer

--- client/src/page/restaurant/OrderOnline.jsx
+++ client/src/page/restaurant/OrderOnline.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { AiOutlineCompass } from "react-icons/ai";
+import { BiTimeFive } from "react-icons/bi";
 
 // components
 import FloatMenuBtn from "../../Components/restaurant/Order-Online/FloatMenuBtn";
+import MenuListContainer from "../../Components/restaurant/Order-Online/MenuListContainer";
 
 const OrderOnline = () => {
   return (
     <>
-      <div className="w-full h-screen flex md:hidden">
-        <FloatMenuBtn />
+      <div className="w-full h-screen flex ">
+        <aside className="hidden md:flex flex-col gap-3 border-r overflow-y-scroll border-gray-200 h-screen w-1/4">
+            <MenuListContainer/>
+        </aside>
+        
       </div>
+      <FloatMenuBtn />
     </>
   );
 };
