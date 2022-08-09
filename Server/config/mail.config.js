987c6a3b4af985c14b09137a4315ade422dddbae
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-08-09 15:32:52 +0530

    add env file

--- Server/config/mail.config.js
+++ Server/config/mail.config.js
@@ -3,6 +3,7 @@ import mailgun from 'mailgun-js';
 export function initializeMailgun(){
     return mailgun({
         apiKey : process.env.MAILGUN__API,
+        
         // domaim : "sandbox56b1ee7f95c3457095f2ad504f3d3631.mailgun.org"
         domain : process.env.MAILGUN__domain
     });
