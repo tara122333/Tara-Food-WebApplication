68fc7eb04b29ea3139215a5d41eaee9598ebea9a
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-08-09 15:32:31 +0530

    add logo

--- Server/config/mail.config.js
+++ Server/config/mail.config.js
@@ -2,8 +2,8 @@ import mailgun from 'mailgun-js';
 
 export function initializeMailgun(){
     return mailgun({
-        apiKey : "cdfc27942d6b8b48119236b7452716d1-2bab6b06-4bb6822f",
+        apiKey : process.env.MAILGUN__API,
         // domaim : "sandbox56b1ee7f95c3457095f2ad504f3d3631.mailgun.org"
-        domain : "sandboxa404e1d14748414b84ad72e7cb07b4fd.mailgun.org"
+        domain : process.env.MAILGUN__domain
     });
 }
