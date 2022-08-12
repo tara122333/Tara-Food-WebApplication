e7667088b083dd4240e2d192ea3ca0bd3a420f5b
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-08-12 22:02:19 +0530

    add new route in review API

--- Server/API/review/index.js
+++ Server/API/review/index.js
@@ -1,6 +1,6 @@
 // Library
 import  express from "express";
-// import passport from "passport";
+import passport from "passport";
 
 // database model
 import { ReviewModel } from "../../database/allModels";
@@ -9,25 +9,46 @@ const Router = express.Router();
 
 
 
+/*
+Route     /
+Des       Get all review
+Params    resid
+BODY      none
+Access    Public
+Method    GET  
+*/
+Router.get("/:resid", async (req, res) => {
+    try {
+      const reviews = await ReviewModel.find({ restaurant: req.params.resid });
+      return res.json({ reviews });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  });
+
+
+
 
 /*
-route      ==> /new 
-method     ==> POST
-Des        ==> add new Food review/rating
-params     ==> none
-Access     ==> public
-body       ==> review object
+Route     /new
+Des       Add new food review/rating
+Params    none
+BODY      review object
+Access    Public
+Method    POST  
 */
-Router.post("/new", async(req,res)=>{
+Router.post("/new", passport.authenticate("jwt"), async (req, res) => {
     try {
-        const {reviewData} = req.body;
-
-        await ReviewModel.create(reviewData);
-        return res.status(200).json({review : "successfully created Reviews"});
+      const { _id } = req.session.passport.user._doc;
+      const { reviewData } = req.body;
+  
+      await ReviewModel.create({ ...reviewData, user: _id });
+  
+      return res.json({ review: "Sucessfully Created Review." });
     } catch (error) {
-        return res.status(500).json({error : error.message});
+      return res.status(500).json({ error: error.message });
     }
-});
+  });
 
 
 
