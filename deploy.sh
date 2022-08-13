6e84bbc03df32d0103375f33de3c2be788e0544d
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2022-08-14 04:36:53 +0530

    fix error

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh "ubuntu@65.1.129.250" "cd app/zomato-clone-2 && sudo docker-compose stop && git pull origin master && sudo docker-compose up -d --build && exit"
+ssh "ubuntu@65.1.129.250" "cd app/tara-food && sudo docker-compose stop && git pull origin main && sudo docker-compose up -d --build && exit"
